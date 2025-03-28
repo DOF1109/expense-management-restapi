5778171844f5e4e46f9f59339f4cb289304f12c1
DOF1109 <friasdaniel.1109@gmail.com>
2025-03-27 23:23:45 -0300

    Refactor ExpenseController to return a list of ExpenseResponse objects

--- src/main/java/com/danielfrias/restapi/controller/ExpenseController.java
+++ src/main/java/com/danielfrias/restapi/controller/ExpenseController.java
@@ -1,13 +1,28 @@
 package com.danielfrias.restapi.controller;
 
+import com.danielfrias.restapi.dto.ExpenseDTO;
+import com.danielfrias.restapi.io.ExpenseResponse;
+import com.danielfrias.restapi.service.ExpenseService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequiredArgsConstructor
 public class ExpenseController {
+    private final ExpenseService expenseService;
+    private final ModelMapper modelMapper;
 
     @GetMapping("/expenses")
-    public String getExpenses() {
-        return "Reading expenses from database";
+    public List<ExpenseResponse> getExpenses() {
+        List<ExpenseDTO> list = expenseService.getAllExpenses();
+        // Convert the list of DTO objects to Response objects
+        List<ExpenseResponse> response = list.stream()
+                .map(expenseDTO -> modelMapper.map(expenseDTO, ExpenseResponse.class))
+                .toList();
+        return response;
     }
 }

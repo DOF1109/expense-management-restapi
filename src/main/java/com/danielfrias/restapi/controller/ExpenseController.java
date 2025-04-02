3fd8f6eca6f6a7f401dd3396f1c8cf704abd1d29
DOF1109 <friasdaniel.1109@gmail.com>
2025-04-02 11:45:43 -0300

    Add logging to ExpenseController and ExpenseServiceImpl for better traceability

--- src/main/java/com/danielfrias/restapi/controller/ExpenseController.java
+++ src/main/java/com/danielfrias/restapi/controller/ExpenseController.java
@@ -4,6 +4,7 @@ import com.danielfrias.restapi.dto.ExpenseDTO;
 import com.danielfrias.restapi.io.ExpenseResponse;
 import com.danielfrias.restapi.service.ExpenseService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,13 +13,16 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class ExpenseController {
     private final ExpenseService expenseService;
     private final ModelMapper modelMapper;
 
     @GetMapping("/expenses")
     public List<ExpenseResponse> getExpenses() {
+        log.info("API GET /expenses called");
         List<ExpenseDTO> list = expenseService.getAllExpenses();
+        log.info("Printing the data from service \n{}", list);
         // Convert the list of DTO objects to Response objects
         List<ExpenseResponse> response = list.stream()
                 .map(expenseDTO -> modelMapper.map(expenseDTO, ExpenseResponse.class))
--- src/main/java/com/danielfrias/restapi/service/impl/ExpenseServiceImpl.java
+++ src/main/java/com/danielfrias/restapi/service/impl/ExpenseServiceImpl.java
@@ -5,6 +5,7 @@ import com.danielfrias.restapi.entity.ExpenseEntity;
 import com.danielfrias.restapi.repository.ExpenseRepository;
 import com.danielfrias.restapi.service.ExpenseService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +13,7 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ExpenseServiceImpl implements ExpenseService {
     private final ExpenseRepository expenseRepository;
     private final ModelMapper modelMapper;
@@ -19,6 +21,7 @@ public class ExpenseServiceImpl implements ExpenseService {
     @Override
     public List<ExpenseDTO> getAllExpenses() {
         List<ExpenseEntity> list = expenseRepository.findAll();
+        log.info("Printing the data from repository \n{}", list);
         // Convert the list of Entity objects to DTO objects
         List<ExpenseDTO> listOfExpenses = list.stream()
                 .map(expenseEntity -> modelMapper.map(expenseEntity, ExpenseDTO.class))
